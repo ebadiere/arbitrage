b22bb86c90b70daf341a3273490f774bc8cd3940
Eric Badiere <ebadiere@gmail.com>
2020-06-27 13:51:24 -0600

    Refactor. Preparing for aave.

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,6 +1,8 @@
-const KyberSwapTokenToToken = artifacts.require('./KyberSwapTokenToToken.sol');
+const KyberSwapTokenToToken = artifacts.require('./Kyber/KyberSwapTokenToToken.sol');
+// const Arbitrageur           = artifacts.require('./Arbitrageur.sol');
 
 module.exports = function(deployer) {
     // kovan for now
    deployer.deploy(KyberSwapTokenToToken, '0x692f391bCc85cefCe8C237C01e1f636BbD70EA4D');
+   // deployer.deploy(Arbitrageur);
 };
