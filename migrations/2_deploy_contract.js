899f75a2f94f0a868fec5a1c5b1ae258d1afe405
Eric Badiere <ebadiere@gmail.com>
2020-06-30 15:22:54 -0600

    WIP. Uniswap v1 and v2 Dai to token, initial checkin.

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,14 @@
 const KyberSwapTokenToToken = artifacts.require('./Kyber/KyberSwapTokenToToken.sol');
-// const Arbitrageur           = artifacts.require('./Arbitrageur.sol');
+const Uni1ToUni2Dai = artifacts.require('./arbitrage/Uni1ToUni2Dai.sol');
+const Uni2ToUni1Dai = artifacts.require('./arbitrage/Uni2ToUni1Dai.sol');
 
-module.exports = function(deployer) {
+
+module.exports = async function(deployer) {
     // kovan for now
-   deployer.deploy(KyberSwapTokenToToken, '0x692f391bCc85cefCe8C237C01e1f636BbD70EA4D');
-   // deployer.deploy(Arbitrageur);
+   await deployer.deploy(KyberSwapTokenToToken, '0x692f391bCc85cefCe8C237C01e1f636BbD70EA4D');
+   // Uniswap below is all mainnet
+   await deployer.deploy(Uni1ToUni2Dai);
+   await deployer.deploy(Uni2ToUni1Dai);
+
+
 };
