303011424f34d0d28e0536cba80b6b7bfa30f93a
Eric Badiere <ebadiere@gmail.com>
2020-07-03 19:57:10 -0600

    WIP. Reduction by declaring the functions.

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -7,8 +7,8 @@ module.exports = async function(deployer) {
     // kovan for now
    await deployer.deploy(KyberSwapTokenToToken, '0x692f391bCc85cefCe8C237C01e1f636BbD70EA4D');
    // Uniswap below is all mainnet
-   await deployer.deploy(Uni1ToUni2Dai);
-   await deployer.deploy(Uni2ToUni1Dai);
+   // await deployer.deploy(Uni1ToUni2Dai);
+   // await deployer.deploy(Uni2ToUni1Dai);
 
 
 };
