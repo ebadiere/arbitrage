6c406b09071022bd8c5ba7b8ee6992c897c5f9d5
Eric Badiere <ebadiere@gmail.com>
2020-07-04 20:01:10 -0600

    Live.

--- migrations/1_deploy_contract.js
+++ migrations/1_deploy_contract.js
@@ -1,7 +1,5 @@
 const Uni1KyberArbitrageur = artifacts.require('./arbitrage/Uni1KyberArbitrageur.sol');
 
-
-
 module.exports = async function(deployer) {
     // kovan for now
    await deployer.deploy(Uni1KyberArbitrageur);
